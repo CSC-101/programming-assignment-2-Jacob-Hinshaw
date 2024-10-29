6d57fdf7e9be6a9258d8f5057081119de2c84499
Jacob-Hinshaw <jrhinsha@calpoly.edu>
2024-10-29 11:01:01 -0700

    Part 1

--- hw2.py
+++ hw2.py
@@ -3,6 +3,27 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
+def create_rectangle(point1:data.Point, point2:data.Point) -> data.Rectangle:
+#    if point1.x >= point2.x and point1.y <= point2.y:
+#        point_top_left = point2
+#        point_bottom_right = point1
+#    if point1.x <= point2.x and point1.y >= point2.y:
+#        point_top_left = point1
+#        point_bottom_right = point2
+#    return data.Rectangle(point_top_left, point_bottom_right)
+    if point1.x >= point2.x:
+        large_x = point1.x
+        small_x = point2.x
+    else:
+        large_x = point2.x
+        small_x = point1.x
+    if point1.y >= point2.y:
+        large_y = point1.y
+        small_y = point2.y
+    else:
+        large_y = point2.y
+        small_y = point1.y
+    return data.Rectangle(data.Point(small_x, large_y), data.Point(large_x, small_y))
 
 
 # Part 2
--- hw2_tests.py
+++ hw2_tests.py
@@ -2,11 +2,26 @@ import data
 import hw2
 import unittest
 
+from data import Rectangle, Point
+
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test1_create_rectangle(self):
+        input1 = data.Point(10,2)
+        input2 = data.Point(2,10)
+        result = hw2.create_rectangle(input1, input2)
+        expected = data.Rectangle(input2, input1)
+        self.assertEqual(expected, result)
+    def test2_create_rectangle(self):
+        input1 = data.Point(2,2)
+        input2 = data.Point(10,10)
+        result = hw2.create_rectangle(input1, input2)
+        expected = Rectangle(Point(2,10), Point(10,2))
+        self.assertEqual(expected, result)
+
 
 
     # Part 2
